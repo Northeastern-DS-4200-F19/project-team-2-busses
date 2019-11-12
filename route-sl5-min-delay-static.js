9ecec823cf16e77e536d9a8fc78dd2ff74df2a38
BreeMadison <breannamadison711@gmail.com>
2019-11-11 22:02:03 -0500

    oh well

--- route-sl5-min-delay-static.js
+++ route-sl5-min-delay-static.js
@@ -1,9 +1,22 @@
-var margin = { top: 50, right: 50, bottom: 50, left: 50 };
+var margin = { top: 54, right: 60, bottom: 107, left: 88 },
+  width = 960 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
 
-var width = 600 - margin.left - margin.right;
-var height = 600 - margin.top - margin.bottom;
+var x = d3.scale.linear().range([0, width]);
 
-//create the svg
+var y = d3.scale.ordinal().rangeRoundBands([0, height]);
+
+var xAxis = d3.svg
+  .axis()
+  .scale(x)
+  .orient("bottom")
+  .ticks(10);
+
+var yAxis = d3.svg
+  .axis()
+  .scale(y)
+  .orient("left");
+
 var svg = d3
   .select("#route-sl5-delay-vis")
   .append("svg")
@@ -12,63 +25,93 @@ var svg = d3
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var x = d3.scaleBand().range([0, width]);
+svg
+  .append("text")
+  .attr("x", 0)
+  .attr("y", -35)
+  .attr("dy", "0.71em")
+  .attr("fill", "#000")
+  .text("Route SL5 Delay")
+  .style("font", "23px avenir")
+  .style("fill", "#000000");
 
-var y = d3.scaleLinear().range([height, 0]);
+svg
+  .append("text")
+  .attr("x", -69)
+  .attr("y", -8)
+  .attr("dy", "0.71em")
+  .attr("fill", "#000")
+  .text("Stop")
+  .style("font", "12px avenir")
+  .style("fill", "#000000")
+  .style("font-weight", "bold");
 
-d3.csv("data/inbound-static-min-delay/route-sl5-inbound-delay.csv").then(function(data) {
-  console.log(data)
 
-  //format the data
-  data.forEach(function(d) {
-    d.Delay = +d.Delay;
-  });
 
-  x.domain(
+
+
+
+
+d3.csv("data/inbound-static-min-delay/route-sl5-inbound-delay.csv", function(error, data) {
+  data.forEach(
+    function(d) {
+      d.Delay = +d.Delay;
+      return d;
+    },
+    function(error, data) {
+      if (error) throw error;
+    }
+  );
+
+  
+
+  x.domain([0, 10]);
+  y.domain(
     data.map(function(d) {
       return d.Stop;
     })
   );
-  y.domain([
-    0,
-    d3.max(data, function(d) {
-      return d.Delay;
-    })
-  ]);
+
+  svg
+    .append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis)
+    .selectAll("text");
+
+  svg
+    .append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end");
+  //       .text("Value ($)");
 
   svg
     .selectAll(".bar")
     .data(data)
     .enter()
     .append("rect")
-    .attr("class", "bar")
-    .attr("x", function(d) {
-      return x(d.Stop);
-    })
-    .attr("width", x.bandwidth())
+    .style("fill", "steelblue")
     .attr("y", function(d) {
-      return y(d.Delay);
+      return y(d.Stop);
     })
-    .attr("height", function(d) {
-      return height - y(d.Delay);
+    .attr("width", function(d) {
+      return x(d.Delay);
     })
-    .attr("fill", "#009e73")
-    .attr("stroke", "#006b4e");
+    .attr("height", 13);
 
-  //add X Axis
   svg
-    .append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
-
-
-
-  //add Y axis
-  svg
-    .append("g")
-    .call(d3.axisLeft(y))
-    
+    .append("text")
+    .attr("x", 350)
+    .attr("y", 370)
+    .attr("dy", "0.71em")
+    .attr("fill", "#000")
+    .text("Delays Between Stops")
+    .style("font", "12px avenir")
+    .style("fill", "#000000")
+    .style("font-weight", "bold");
 });
