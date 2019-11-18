b5272772c505d5f295421adb745a8359920c6aaa
BreeMadison <breannamadison711@gmail.com>
2019-11-18 16:22:08 -0500

    buttons

---
+++ filters.js
@@ -0,0 +1,17 @@
+// These scripts control the filtering 
+function selectAll() {
+    var items = document.getElementsByName('filter-btn');
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].type == 'checkbox')
+            items[i].checked = true;
+    }
+}
+
+function UnSelectAll() {
+    var items = document.getElementsByName('filter-btn');
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].type == 'checkbox')
+            items[i].checked = false;
+    }
+}
+
